708aa2696a90486b6b55bbd20bc290662ecd451b
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-08-11 16:43:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const statusElement = document.getElementById("status");
+    const enterButton = document.getElementById("enterBtn");
+    
+    enterButton.addEventListener("click", function() {
+        statusElement.innerHTML = "<h1>Entered Metaverse</h1>";
+    });
+});
